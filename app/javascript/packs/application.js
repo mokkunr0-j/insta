1be8fc8a6f81fc2f04dd6644b238a38b44cdb88e
jun yamada <jacqk.rv@gmail.com>
2020-11-11 10:43:36 +0900

    [update] application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+require("jquery/dist/jquery.js")
 require("bootstrap/dist/js/bootstrap.min").start()
+require("channels")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
